e287fd065a40bec8b020c1e962f5da3b796804c0
Mahadi Hasan <mahadih137@gmail.com>
2023-10-31 17:58:54 +0600

    cookie parser & cors middleware updated

--- car-doctor-server/server.js
+++ car-doctor-server/server.js
@@ -11,7 +11,13 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["*", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use((req, res, next) => {
   req.headers = {
     "access-control-allow-origin": "*",
