b5877b26cafb890e520ec26f48a31384d223a40a
Mahadi Hasan <mahadih137@gmail.com>
2023-10-31 22:21:43 +0600

    dkjflkdflk

--- car-doctor-server/server.js
+++ car-doctor-server/server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.vdnpxvd.mongodb.net/?retryWrites=true&w=majority`;
 
